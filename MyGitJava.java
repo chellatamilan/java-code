89a01980da8407f983e5c6133226cb69f5f8b89a
Chellatamilan <chellatamilan@gmail.com>
2024-03-10 17:09:17 +0530

    Thirs Commit

--- MyGitJava.java
+++ MyGitJava.java
@@ -3,7 +3,7 @@ public class MyGitJava
 	public static void main(String[] args) 
 	{
 		System.out.println("------------------------------");
-	        System.out.println("Hello World!..From VIT GitHub.....");
+	        System.out.println("Hello World!..From VIT Chella GitHub.....");
 		System.out.println("------------------------------");
 		
 	}
